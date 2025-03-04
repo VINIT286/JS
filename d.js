69b8c09097f015b3952b9f3cde6697edd3bcdc44
VINIT286 <vinitgandhe10301@gmail.com>
2025-03-04 15:42:59 +0530

    add files

---
+++ d.js
@@ -0,0 +1,23 @@
+const ch =(n)=>{
+     if(n==1){
+        let big = document.querySelector('#big');
+        big.src = "https://imgd.aeplcdn.com/1056x594/n/cw/ec/171297/z900-right-side-view-2.png?isig=0&q=80&wm=3"
+     }
+     else if(n==2){
+        let big = document.querySelector('#big');
+        big.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2er6npP4sXfx6JSddGyGiOGg3sFRxXHXVog&s"
+
+     }
+
+     else if(n==3){/
+        let big = document.querySelector('#big');
+        big.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJWW3Lc3DAUA_eMHN-J8cLj0fNcZlb27hbXg&s"
+
+     }
+     else if(n==4) {
+        let big = document.querySelector('#big');
+        big.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1ANhlOkjg8IZQfdSCiiACqXERk0lAzukv1g&s"
+
+     }
+     
+}
