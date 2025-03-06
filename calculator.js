5e91194266a798adb2b8b2160a010645de40f3cc
VINIT286 <vinitgandhe10301@gmail.com>
2025-03-06 12:55:36 +0530

    add files

--- calculator.js
+++ calculator.js
@@ -11,3 +11,21 @@ const cal =()=>{
     show.innerHTML = eval(store);
     store =eval(store);
 }
+const del=()=>{
+    store = store.slice(0,store.length-1);
+    let show =document.querySelector('#show')
+    show.innerHTML=store;
+}
+
+const ac =() =>{
+    store='';
+    letshow=document.querySelector('#show');
+    show.innerHTML = store;
+
+}
+
+const square=() =>{
+    store= eval(store*store);
+    letshow=document.querySelector('#show');
+    show.innerHTML = store;
+}
