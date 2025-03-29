da8faf033497c2d743ae258c16f691b8e8f38c75
VINIT286 <vinitgandhe10301@gmail.com>
2025-03-29 12:07:41 +0530

    add files

---
+++ form.js
@@ -0,0 +1,17 @@
+const cl=()=>{
+let nm=document.querySelector('#nm').value;
+let em=document.querySelector('#em').value;
+let num=document.querySelector('#num').value;
+let im=document.querySelector('#im').value;
+
+let tl=`<div>
+<div><img src="${im}"</div>
+<div> name =${nm}</div>
+<div>email=${em}</div>
+<div>mno.=${num}</div>
+<div>`
+let df=document.querySelector('#df')
+df.innerHTML=tl
+console.log(nm,em,num,im)
+return false;
+}
