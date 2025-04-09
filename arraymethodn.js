04a556edc5e43d657a853bf666968a859f778b70
VINIT286 <vinitgandhe10301@gmail.com>
2025-04-09 13:52:42 +0530

    add files

---
+++ arraymethodn.js
@@ -0,0 +1,8 @@
+let arr=[4,6,3,6,7,855,74,3,7,66,77]
+console.log(arr.length)
+console.log(arr.slice(2,5))
+console.log(arr.slice(0.5))
+console.log(arr.slice(2,arr.length-1))
+console.log(arr.slice(0,6))
+let op=document.querySelector("#op")
+op.innerHTML=arr
--- filter2.js
+++ filter2.js
@@ -1,41 +1,75 @@
 
 let cr = [
     {
-        "ProductName": "iPhone 11",
-        "ProductBrand": "Apple",
-        "ProductPrice": 25000,
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtMJ0wayMYgSPs0nVPngjHwuW8w1vfHSD0WQ&s"
+        image: "https://m.media-amazon.com/images/I/41R51TjCz7L._MCnd_AC_.jpg",
+        "ProductName": "Lava Storm 5G",
+        "ProductBrand": "Lava",
+        "ProductPrice": 10999
     },
     {
+        image: "https://m.media-amazon.com/images/I/41V7f38oPIL._MCnd_AC_.jpg",
         "ProductName": "iPhone 12",
         "ProductBrand": "Apple",
-        "ProductPrice": 35000,
-        image: "https://rukminim3.flixcart.com/image/850/1000/kg8avm80/mobile/j/f/9/apple-iphone-12-dummyapplefsn-original-imafwg8dkyh2zgrh.jpeg?q=20&crop=false"
+        "ProductPrice": 35000
     },
     {
+        image: "https://m.media-amazon.com/images/I/41tnZF1HuXL._MCnd_AC_.jpg",
         "ProductName": "iPhone 13",
         "ProductBrand": "Apple",
-        "ProductPrice": 45000,
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgkGSaLQZ3RpJGCY3_4dUml0FIGyIBdl44WQ&s"
+        "ProductPrice": 45000
     },
     {
+        image: "https://m.media-amazon.com/images/I/41UIAaFDznL._MCnd_AC_.jpg",
         "ProductName": "iPhone 14",
         "ProductBrand": "Apple",
-        "ProductPrice": 55000,
-        image: "https://m.media-amazon.com/images/I/61bK6PMOC3L._AC_UF1000,1000_QL80_.jpg"
+        "ProductPrice": 55000
     },
     {
+        image: "https://m.media-amazon.com/images/I/41-mPer5YLL._MCnd_AC_.jpg",
         "ProductName": "iPhone 15",
         "ProductBrand": "Apple",
-        "ProductPrice": 65000,
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSvgCPBUySTN1TjyFl-Lu-XqGoZlDnLDpnbQ&s"
+        "ProductPrice": 65000
     },
     {
-        "ProductName": "iPhone 16",
+        image: "https://m.media-amazon.com/images/I/41Cd8ElGw8L._MCnd_AC_.jpg",
+        "ProductName": "samsung",
         "ProductBrand": "Apple",
-        "ProductPrice": 100000,
-        image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcStvAWT8L9cuTxzDGIhlX7MDYAVemsh4T660V9c6XZgYdCRZXgyILsnYGxwFsbuxYzeE3Fez0vRqFvi-SxRf_jBtS0d5ioL9FBgIn7ENRM18wEb3rMymodYiap47HyT_XdjyLnt7mk&usqp=CAc"
-    }
+        "ProductPrice": 100000
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/41USEALw9uL._MCnd_AC_.jpg",
+        "ProductName": "iPhone 12",
+        "ProductBrand": "Apple",
+        "ProductPrice": 35000
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/415IkCS68OL._MCnd_AC_.jpg",
+        "ProductName": "iPhone 12",
+        "ProductBrand": "Apple",
+        "ProductPrice": 35000
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/41QioujvLVL._MCnd_AC_.jpg",
+        "ProductName": "iPhone 12",
+        "ProductBrand": "Apple",
+        "ProductPrice": 35000
+    },  {
+        image: "https://m.media-amazon.com/images/I/41GJnqibXjL._MCnd_AC_.jpg",
+        "ProductName": "iPhone 12",
+        "ProductBrand": "Apple",
+        "ProductPrice": 35000
+    },  {
+        image: "https://m.media-amazon.com/images/I/51vu7nKLeWL._MCnd_AC_.jpg",
+        "ProductName": "iPhone 12",
+        "ProductBrand": "Apple",
+        "ProductPrice": 35000
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/31uajoTE1jL._MCnd_AC_.jpg",
+        "ProductName": "iPhone 12",
+        "ProductBrand": "Apple",
+        "ProductPrice": 35000
+    },
 ];
 const s =()=>{
     
@@ -57,10 +91,10 @@ const s =()=>{
     let output = document.querySelector('#object');
     output.innerHTML = store.map(a => `
         <div class="card">
+            <div><img src="${a.image}" width="100px" height="100px"></div>
             <div class="name">${a.ProductName}</div>
             <div class="brand">${a.ProductBrand}</div>
             <div class="price">${a.ProductPrice}</div>
-            <div><img src="${a.image}" width="100px" height="100px"></div>
         </div>
     `).join(" ");
     return false;
@@ -70,10 +104,10 @@ const a = ()=>{
     let output = document.querySelector('#object');
     output.innerHTML = cr.map(a => `
         <div class="card">
+            <div><img id="i" src="${a.image}" width="100px" height="100px"></div>
             <div class="name">${a.ProductName}</div>
             <div class="brand">${a.ProductBrand}</div>
             <div class="price">${a.ProductPrice}</div>
-            <div><img src="${a.image}" width="100px" height="100px"></div>
         </div>
     `).join(" ");
     return false;
