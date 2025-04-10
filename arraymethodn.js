3e881573ab97ffe8b55894afd117f170f2ae5a7f
VINIT286 <vinitgandhe10301@gmail.com>
2025-04-10 14:23:26 +0530

    add files

--- arraymethodn.js
+++ arraymethodn.js
@@ -1,8 +1,32 @@
-let arr=[4,6,3,6,7,855,74,3,7,66,77]
-console.log(arr.length)
-console.log(arr.slice(2,5))
-console.log(arr.slice(0.5))
-console.log(arr.slice(2,arr.length-1))
-console.log(arr.slice(0,6))
-let op=document.querySelector("#op")
-op.innerHTML=arr
+ let arr=[1,2,3,4,5,6,7,8,9,10]
+// console.log(arr.length)
+// console.log(arr.slice(2,5))
+// console.log(arr.slice(0.5))
+// console.log(arr.slice(2,arr.length-1))
+// console.log(arr.slice(0,6))
+// let op=document.querySelector("#op")
+// op.innerHTML=arr
+
+// arr.push(333)
+// console.log(arr)
+
+// arr.pop()
+// console.log(arr)
+
+// arr.shift()
+// console.log(arr)
+
+// arr.unshift(100)
+// console.log(arr)
+
+// arr.splice(0,1)
+// console.log(arr)
+
+// arr.splice(arr.length-1,1)
+// console.log(arr)
+
+// arr.splice(arr.length,0,5,5)
+// console.log(arr)
+
+// arr.splice(0,0,3434,4343)
+// console.log(arr)
